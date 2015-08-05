5b3553686b3a735b8ffaeb5a189e0e147eb0851f
LaVieEnRoux <j.smith@acadiau.ca>
2015-08-05 19:21:45 -0300

    Beginnings of basic processing funcionality

--- get_image.py
+++ get_image.py
@@ -1,21 +1,50 @@
 import wget
 import sys
+import cv2
 
 def grabImage(num_images):
     '''
     Grabs num_images images from the human_URLs.txt file
+
+    Returns a list of filenames of the image files.
     '''
     f = open('human_URLs.txt', 'r')
     i = 0
+    filenames = []
+
+    # iterate through the defined number of image files
     for line in f:
         if i == num_images:
             break
-        wget.download(line)
+        filenames.append(wget.download(line))
         i += 1
 
+    return filenames
+
+
+def processFile(filename):
+    '''
+    Performs some kind of as of yet mildly undefined processing on the photo.
+    '''
+
+    # read in via opencv
+    img = cv2.imread(filename)
+    rows = img.shape[0]
+    cols = img.shape[1]
+
+    # do something!
+
+    # return something else!
+
+
 if __name__ == '__main__':
+
     if len(sys.argv) == 1:
         num_images = 1
     else:
         num_images = int(sys.argv[1])
-    grabImage(num_images)
+    filenames = grabImage(num_images)
+
+    # do processing right here
+    for f in filenames:
+        f_edit = processFile(f)
