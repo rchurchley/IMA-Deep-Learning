714914286ba5ca3c4947f5e2d1acbb93e9ff60b5
LaVieEnRoux <j.smith@acadiau.ca>
2015-08-07 16:56:10 -0300

    Adding filter functionality, not detecting faces yet

--- get_all_images.py
+++ get_all_images.py
@@ -3,17 +3,18 @@ from deepsix.collection import *
 
 if __name__ == '__main__':
     human_urls = deepsix.collection.flickr.urls_tagged(
-        'human',
-        max_images=4,
+        'face',
+        max_images=20,
         apikey='Flickr_API_key.txt'
     )
 
-    human_urls += deepsix.collection.imagenet.urls_tagged(
-        'n07942152',
-        max_images=5
-    )
+    # human_urls += deepsix.collection.imagenet.urls_tagged(
+    #     'n07942152',
+    #     max_images=5
+    # )
 
     deepsix.collection.get_images_from_urls(
         human_urls,
-        output_folder='downloads'
+        output_folder='downloads',
+        filter_type='face'
     )
