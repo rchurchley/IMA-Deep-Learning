3a8f4a129493768d31d58b581fe4fd956ddab1ae
Ross Churchley <ross@rosschurchley.com>
2015-08-07 11:55:59 -0500

    Move all modules involving collecting images from the internet to a subpackage deepsix.collection

---
+++ deepsix/collection/__init__.py
@@ -0,0 +1,5 @@
+# define subpackages loaded when you call `from deepsix.collection import * `
+__all__ = ['flickr', 'imagenet']
+
+# make `get_images_from_urls` available under `deepsix.collection` namespace
+from downloader import get_images_from_urls
--- get_all_images.py
+++ get_all_images.py
@@ -5,12 +5,11 @@ Required packages
 - wget
 """
 
-import deepsix.flickr
-import deepsix.imagenet
-import deepsix.download
+import deepsix.collection
+from deepsix.collection import *
 
 if __name__ == '__main__':
-    flickr = deepsix.flickr.session('Flickr_API_key.txt')
-    human_urls = deepsix.flickr.urls_with_keywords(flickr, 'human', 4)
-    human_urls += deepsix.imagenet.urls_with_synset('n07942152', 5)
-    deepsix.download.get_images_from_urls(human_urls, output_folder='downloads')
+    flickr = deepsix.collection.flickr.session('Flickr_API_key.txt')
+    human_urls = deepsix.collection.flickr.urls_with_keywords(flickr, 'human', 4)
+    human_urls += deepsix.collection.imagenet.urls_with_synset('n07942152', 5)
+    deepsix.collection.get_images_from_urls(human_urls, output_folder='downloads')
