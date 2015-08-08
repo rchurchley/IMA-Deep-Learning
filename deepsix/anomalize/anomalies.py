f8898ca4ef62c1e0920c2b30a89944ef096af62e
Ross Churchley <ross@rosschurchley.com>
2015-08-07 19:50:33 -0500

    Add random white lines to images using PIL

---
+++ deepsix/anomalize/anomalies.py
@@ -0,0 +1,32 @@
+from PIL import Image, ImageDraw
+import numpy.random as rand
+import os
+
+
+def add_random_line(filename, output_filename, min_thickness, max_thickness):
+    img = Image.open(filename)
+    draw = ImageDraw.Draw(img)
+    # define random vertices for the rectilinear path to draw
+    points = [
+        (rand.randint(0, img.size[0]), rand.randint(0, img.size[1])),
+        (rand.randint(0, img.size[0]), rand.randint(0, img.size[1]))
+    ]
+    color = (255, 255, 255)
+    width = rand.randint(min_thickness, max_thickness)
+    draw.line(points, fill=color, width=width)
+    del draw
+
+    img.save(output_filename, "JPEG")
+
+
+def add_random_lines(input_directory='images/thumbnails',
+                     output_directory='images/anomalized',
+                     min_thickness=10,
+                     max_thickness=40):
+    for filename in os.listdir(input_directory):
+        root, ext = os.path.splitext(filename)
+        if ext == '.jpg' or ext == '.jpeg':
+            add_random_line(input_directory + "/" + filename,
+                            output_directory + "/" + filename,
+                            min_thickness,
+                            max_thickness)
--- get_all_images.py
+++ get_all_images.py
@@ -1,10 +1,12 @@
 import deepsix.collection
 from deepsix.collection import *
+import deepsix.anomalize
+from deepsix.anomalize import *
 
 if __name__ == '__main__':
     human_urls = deepsix.collection.flickr.urls_tagged(
         'face',
-        max_images=5,
+        max_images=20,
         apikey='Flickr_API_key.txt'
     )
 
@@ -15,3 +17,4 @@ if __name__ == '__main__':
 
     deepsix.collection.get_images_from_urls(human_urls)
     deepsix.collection.make_square_thumbnails()
+    deepsix.anomalize.add_random_lines()
