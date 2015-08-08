436103adb29bdc614d34d5be116c7af3a998896d
Ross Churchley <ross@rosschurchley.com>
2015-08-08 17:12:22 -0500

    Minor bugfix

--- get_dataset.py
+++ get_dataset.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
         'person',
         max_images=40,
         apikey='Flickr_API_key.txt',
-        debug=debug
+        debug=True
     )
 
     # Download images, crop and resize them, and generate 'bad' copies.
