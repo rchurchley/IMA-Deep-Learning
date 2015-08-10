a451f44a76183bc7f8797aa8ca13a6b038b40ec3
Ross Churchley <ross@rosschurchley.com>
2015-08-10 14:39:09 -0500

    Download from keywords given from the CLI

--- download_images.py
+++ download_images.py
@@ -3,16 +3,18 @@ from deepsix.collection import *
 import sys
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
+    if len(sys.argv) < 3:
+        keywords = 'person'
         count = 10
     else:
-        count = int(sys.argv[1])
+        keywords = sys.argv[1]
+        count = int(sys.argv[2])
 
     # Get a list of URLs of images to download.
-    human_urls = deepsix.collection.flickr.urls_tagged(
-        'person',
+    urls = deepsix.collection.flickr.urls_tagged(
+        keywords,
         api_key='Flickr_API_key.txt')
 
-    deepsix.collection.get_images_from_urls(human_urls,
+    deepsix.collection.get_images_from_urls(urls,
                                             max_count=count,
                                             output_directory='images/raw')
