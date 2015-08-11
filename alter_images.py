54e7282ad4217501012551410817f4a95e5de6fb
Ross Churchley <ross@rosschurchley.com>
2015-08-11 17:13:56 -0500

    Add rectangles by default

--- alter_images.py
+++ alter_images.py
@@ -6,13 +6,13 @@ import sys
 if __name__ == '__main__':
     if len(sys.argv) < 3:
         input_directory = 'images/64'
-        output_directory = 'images/64-random-circle'
+        output_directory = 'images/64-random-rectangle'
     else:
         input_directory = sys.argv[1]
         output_directory = sys.argv[2]
 
     deepsix.collection.alter_images(
-        procedure=deepsix.anomalies.add_circle,
+        procedure=deepsix.anomalies.add_rectangle,
         args=16,
         input_directory=input_directory,
         output_directory=output_directory,
